d230c5ef145e518684e6dd9dbc0afd3fc535b41f
Akimov Max <maxymus2828@gmail.com>
2026-05-07 00:23:54 +0300

    created catch-all routes

---
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -0,0 +1,104 @@
+"use client";
+import SearchBox from "@/components/SearchBox/SearchBox";
+import css from "./NotesClient.module.css";
+import { useDeleteNote } from "@/hooks/useDeleteNote";
+import { useNotes } from "@/hooks/useNotes";
+import { usePostNote } from "@/hooks/usePostNote";
+import { PostNote } from "@/lib/api";
+import { useState } from "react";
+import { Toaster } from "react-hot-toast";
+import Modal from "@/components/Modal/Modal";
+import NoteForm from "@/components/NoteForm/NoteForm";
+import Loader from "@/components/Loader/Loader";
+import ErrorMessage from "@/components/ErrorMessage/ErrorMessage";
+import NoteList from "@/components/NoteList/NoteList";
+import Pagination from "@/components/Pagination/Pagination";
+import { NoteTag } from "@/types/note";
+
+type NotesClientProps = {
+  tag?: NoteTag;
+};
+
+export default function NotesClient({ tag }: NotesClientProps) {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const { data, error, isSuccess, isFetching, isLoading, isError, refetch } =
+    useNotes(searchQuery, currentPage, tag);
+
+  const [isRetrying, setIsRetrying] = useState(false);
+
+  const postNoteMutation = usePostNote();
+
+  const deleteNoteMutation = useDeleteNote();
+
+  const totalPages = data?.totalPages ?? 0;
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleError = async () => {
+    setIsRetrying(true);
+    await refetch();
+    setIsRetrying(false);
+  };
+
+  const handleNoteAdd = (formValues: PostNote | null) => {
+    if (!formValues) return null;
+
+    postNoteMutation.mutate(formValues);
+    handleModalClose();
+  };
+
+  const handleNoteDelete = (id: string) => {
+    deleteNoteMutation.mutate(id);
+  };
+
+  return (
+    <>
+      <Toaster />
+      <header className={css.toolbar}>
+        <button className={css.button} onClick={handleModalOpen}>
+          Create note +
+        </button>
+        <SearchBox value={searchQuery} onChange={setSearchQuery} />
+      </header>
+      {isModalOpen && (
+        <Modal onClose={handleModalClose}>
+          <NoteForm onSubmit={handleNoteAdd} onClose={handleModalClose} />
+        </Modal>
+      )}
+      {isLoading && isFetching && <Loader />}
+      {isError && (
+        <ErrorMessage
+          message={error.message}
+          onClick={handleError}
+          isRetrying={isRetrying}
+        />
+      )}
+      {isSuccess && data && data.notes.length > 0 && (
+        <NoteList notes={data?.notes} onClick={handleNoteDelete} />
+      )}
+
+      {data && data.notes.length === 0 && (
+        <ErrorMessage
+          message={"Notes not found"}
+          onClick={handleError}
+          isRetrying={isRetrying}
+        />
+      )}
+      {data && data.notes.length > 0 && (
+        <Pagination
+          pageCount={totalPages}
+          onPageChange={({ selected }) => setCurrentPage(selected + 1)}
+          forcePage={currentPage - 1}
+        />
+      )}
+    </>
+  );
+}
---
+++ app/notes/filter/[...slug]/page.tsx
@@ -0,0 +1,37 @@
+import css from "./page.module.css";
+import { fetchNotes } from "@/lib/api";
+import { NoteTag } from "@/types/note";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import NotesClient from "./Notes.client";
+
+type Props = {
+  params: Promise<{ slug: string[] }>;
+};
+
+export default async function NotesByTag({ params }: Props) {
+  const queryClient = new QueryClient();
+  const { slug } = await params;
+  const tagParam = slug?.[0] ?? "all";
+  const isAll = tagParam === "all";
+
+  const tag = isAll ? undefined : (tagParam as NoteTag);
+
+  //   const notes = await fetchNotes(tag);
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", tag ?? "all"],
+    queryFn: () => fetchNotes(tag),
+  });
+
+  return (
+    <>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <NotesClient tag={tag} />
+      </HydrationBoundary>
+    </>
+  );
+}

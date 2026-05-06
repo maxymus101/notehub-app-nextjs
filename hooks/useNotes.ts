211040c11284a3b72adb82881d819e8e648c03e8
Akimov Max <maxymus2828@gmail.com>
2026-05-07 00:25:03 +0300

    modified

--- hooks/useNotes.ts
+++ hooks/useNotes.ts
@@ -1,10 +1,16 @@
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { fetchNotes } from "../lib/api";
+import { NoteTag } from "@/types/note";
 
-export const useNotes = (searchQuery: string, currentPage: number) => {
+export const useNotes = (
+  searchQuery: string,
+  currentPage: number,
+  tag?: NoteTag | undefined,
+  perPage = 12,
+) => {
   return useQuery({
-    queryKey: ["notes", searchQuery, currentPage],
-    queryFn: () => fetchNotes(searchQuery, currentPage),
+    queryKey: ["notes", searchQuery, tag ?? "all", currentPage],
+    queryFn: () => fetchNotes(searchQuery, tag, currentPage, perPage),
     placeholderData: keepPreviousData,
   });
 };

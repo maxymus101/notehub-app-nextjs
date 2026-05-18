c1f1e11d0b0312c9bfcbe0a335753c1572c1cd23
Akimov Max <maxymus2828@gmail.com>
2026-05-19 01:09:53 +0300

    new route - create

---
+++ app/(public routes)/notes/actions/create/page.tsx
@@ -0,0 +1,12 @@
+import css from "./page.module.css";
+
+export default function CreateNote() {
+  return (
+    <>
+      <div className={css.container}>
+        <h1 className={css.title}>Create note</h1>
+        {/* NoteForm component */}
+      </div>
+    </>
+  );
+}

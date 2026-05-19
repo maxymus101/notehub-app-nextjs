0890dffd9ca9bb1c3482cc11cf4030a9feba1c99
Akimov Max <maxymus2828@gmail.com>
2026-05-19 20:00:32 +0300

    added new route

--- app/(public routes)/notes/actions/create/page.tsx
+++ app/(public routes)/notes/actions/create/page.tsx
@@ -1,11 +1,32 @@
+import NoteForm from "@/components/NoteForm/NoteForm";
 import css from "./page.module.css";
+
+export async function generateMetadata() {
+  return {
+    title: "Create new note",
+    description: "Create new note with adding title, content and tag.",
+    openGraph: {
+      title: "Create new note",
+      description: "Create new note with adding title, content and tag.",
+      url: "https://notehub-app-nextjs.vercel.app/notes/action/create",
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: "NoteHub",
+        },
+      ],
+    },
+  };
+}
 
 export default function CreateNote() {
   return (
     <>
       <div className={css.container}>
         <h1 className={css.title}>Create note</h1>
-        {/* NoteForm component */}
+        <NoteForm />
       </div>
     </>
   );

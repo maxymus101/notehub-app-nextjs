b0a6a8323721c07b883a662499e2da0d4e87cc3b
Akimov Max <maxymus2828@gmail.com>
2026-05-19 19:59:24 +0300

    added img remotePatern

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "https://ac.goit.global" }],
+  },
 };
 
 export default nextConfig;
